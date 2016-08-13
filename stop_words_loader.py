351293fe4323dde3ec119a0346d837c7bc0a00e5
balolam <balolam@gmail.com>
2016-08-13 09:37:36 +0300

    Добавлен список стоп-слов

---
+++ stop_words_loader.py
@@ -0,0 +1,15 @@
+class StopWordsLoader(object):
+    def __init__(self, file_name):
+        self.file_name = file_name
+
+    def get(self):
+        words = []
+
+        f = open(self.file_name, 'r')
+
+        for line in f:
+            words.append(line.strip())
+
+        f.close()
+
+        return list(set(words))
